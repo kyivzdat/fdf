e6b2d6bb22b2355202ca129fa6678946eb4ab990
Vladyslav PALAMARCHUK <vpalamar@e1r7p13.unit.ua>
2019-09-04 15:35:20 +0300

    reworked move logic beyond the area

--- main.c
+++ main.c
@@ -21,48 +21,32 @@ int	key_hook(int key, void *param)
 
 	if (key == 124)
 	{
-		if ((coord_x(all, all->gnrl.maxPointX->x, all->gnrl.maxPointX->y) + 10 < 1000 && all->gnrl.proection == 1) || 
-		(no_iso_coord(all, all->gnrl.maxPointX->x, 1) + 10 < 1000 && all->gnrl.proection == 0))
+		if ((coord_x(all, all->gnrl.minPointX->x, all->gnrl.minPointX->y) < 1000 && all->gnrl.proection == 1) || 
+		(no_iso_coord(all, all->gnrl.minPointX->x, 1) < 1000 && all->gnrl.proection == 0))
 			all->gnrl.midx += 10;
 	}
 	else if (key == 123)
 	{
-		if ((coord_x(all, all->gnrl.minPointX->x, all->gnrl.minPointX->y) - 10 > 0 && all->gnrl.proection == 1) || 
-		(no_iso_coord(all, all->gnrl.minPointX->x, 1) - 10 > 0 && all->gnrl.proection == 0))
+		if ((coord_x(all, all->gnrl.maxPointX->x, all->gnrl.maxPointX->y) > 0 && all->gnrl.proection == 1) || 
+		(no_iso_coord(all, all->gnrl.maxPointX->x, 1) > 0 && all->gnrl.proection == 0))
 			all->gnrl.midx -= 10;
 	}
 	else if (key == 126)
 	{
-		if ((coord_y(all, all->gnrl.minPointY->x, all->gnrl.minPointY->y, all->gnrl.minPointY->z) - 10 > 0
-		&& all->gnrl.proection == 1) || (no_iso_coord(all, all->gnrl.minPointY->y, 0) - 10 > 0 && all->gnrl.proection == 0))
+		if ((coord_y(all, all->gnrl.maxPointY->x, all->gnrl.maxPointY->y, all->gnrl.maxPointY->z) > 0
+		&& all->gnrl.proection == 1) || (no_iso_coord(all, all->gnrl.maxPointY->y, 0) > 0 && all->gnrl.proection == 0))
 			all->gnrl.midy -= 10;
 	}
 	else if (key == 125)
 	{
-		if ((coord_y(all, all->gnrl.maxPointY->x, all->gnrl.maxPointY->y, all->gnrl.maxPointY->z) + 10 < 1000
-		&& all->gnrl.proection == 1) || (no_iso_coord(all, all->gnrl.maxPointY->y, 0) + 10 < 1000 && all->gnrl.proection == 0))
+		if ((coord_y(all, all->gnrl.minPointY->x, all->gnrl.minPointY->y, all->gnrl.minPointY->z) < 1000
+		&& all->gnrl.proection == 1) || (no_iso_coord(all, all->gnrl.minPointY->y, 0) < 1000 && all->gnrl.proection == 0))
 			all->gnrl.midy += 10;
 	}
 	else if (key == 69 || key == 24)
-	{
-		if ((coord_x(all, all->gnrl.maxPointX->x, all->gnrl.maxPointX->y) + 25 < 1000 &&
-		coord_x(all, all->gnrl.minPointX->x, all->gnrl.minPointX->y) - 25 > 0 &&
-		coord_y(all, all->gnrl.minPointY->x, all->gnrl.minPointY->y, all->gnrl.minPointY->z) - 25 > 0 &&
-		coord_y(all, all->gnrl.maxPointY->x, all->gnrl.maxPointY->y, all->gnrl.maxPointY->z) + 25 < 1000 &&
-		all->gnrl.proection == 1) ||
-		(no_iso_coord(all, all->gnrl.maxPointX->x, 1) + 40 < 1000 &&
-		no_iso_coord(all, all->gnrl.maxPointY->y, 0) + 40 < 1000 && all->gnrl.proection == 0))
 			all->gnrl.scale += 2;
-	}
 	else if (key == 78 || key == 27)
-	{
-		if (all->gnrl.scale - 2 > 0 && ((coord_x(all, all->gnrl.maxPointX->x, all->gnrl.maxPointX->y) - 25 > 0 &&
-		coord_x(all, all->gnrl.minPointX->x, all->gnrl.minPointX->y) + 25 < 1000 &&
-		coord_y(all, all->gnrl.minPointY->x, all->gnrl.minPointY->y, all->gnrl.minPointY->z) + 25 < 1000 &&
-		coord_y(all, all->gnrl.maxPointY->x, all->gnrl.maxPointY->y, all->gnrl.maxPointY->z) - 25 > 0 &&
-		all->gnrl.proection == 1) || all->gnrl.proection == 0)) 
 			all->gnrl.scale -= 2;
-	}
 	draw(all);
 	return (0);
 }
